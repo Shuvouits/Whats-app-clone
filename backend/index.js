fefe69039fe658f5477e08f2f3737532c6b02f2e
Shuvouits <shuvobhowmik_cse11uits@yahoo.com>
2024-09-14 23:09:52 +0600

    Connect mongo db

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import app from "./app.js";
 import logger from "./configs/logger.config.js";
 
@@ -6,9 +7,58 @@ import logger from "./configs/logger.config.js";
 dotenv.config();
 
 //env variables
+const { DATABASE_URL } = process.env;
+
+//env variables
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  
+//exit on mognodb error
+mongoose.connection.on("error", (err) => {
+  logger.error(`Mongodb connection error : ${err}`);
+  process.exit(1);
+});
+
+//mongodb debug mode
+if (process.env.NODE_ENV !== "production") {
+  mongoose.set("debug", true);
+}
+
+//mongodb connection
+mongoose
+  .connect(DATABASE_URL, {
+    //useNewUrlParser: true,
+    //useUnifiedTopology: true,
+  })
+  .then(() => {
+    logger.info("Connected to Mongodb.");
+  });
+
+let server;
+
+server = app.listen(PORT, () => {
   logger.info(`Server is listening at ${PORT}...`);
+});
+
+const exitHandler = () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+//SIGTERM
+process.on("SIGTERM", () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  }
 });
