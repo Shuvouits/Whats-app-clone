541ce6ba3f5378db3b9081adf2079c1c38550d4d
Shuvouits <shuvobhowmik_cse11uits@yahoo.com>
2024-09-14 09:45:50 +0600

    section-4 . manage backend project structure

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,10 @@
-console.log("shuvo");
+import express from "express";
+
+//create express app
+const app = express();
+
+app.post("/test", (req, res) => {
+  res.send("hello from server");
+});
+
+export default app;
